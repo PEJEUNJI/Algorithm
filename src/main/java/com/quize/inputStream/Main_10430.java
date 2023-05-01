295f9f4be976706997dfa4d8010b0acf1038a77e
eunji.park <pej870922@gmail.com>
2023-05-01 16:56:44 +0200

    quize

---
+++ src/main/java/com/quize/inputStream/Main_10430.java
@@ -0,0 +1,30 @@
+package com.quize.inputStream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author https://st-lab.tistory.com/19
+ *
+ */
+public class Main_10430 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append((A+B)%C).append("\n");
+		sb.append(((A%C) + (B%C))%C).append("\n");
+		sb.append((A*B)%C).append("\n");
+		sb.append(((A%C) * (B%C))%C);
+		System.out.print(sb);
+	}
+}

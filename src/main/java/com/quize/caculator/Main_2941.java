976515d1847c5d14c498be64769e8f71b27b7a24
eunji.park <pej870922@gmail.com>
2023-05-24 14:55:35 +0200

    add comment

--- src/main/java/com/quize/caculator/Main_2941.java
+++ src/main/java/com/quize/caculator/Main_2941.java
@@ -3,7 +3,11 @@ package com.quize.caculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+/**
+ * 
+ * @author https://www.acmicpc.net/problem/2941
+ *
+ */
 public class Main_2941 {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -14,6 +18,8 @@ public class Main_2941 {
 		int length = inputStr.length();
 		for (int index = 0; index < length;) {
 			boolean match = false;
+			//Checking if any characters belong to the special
+			//The length of the special character is 2 or 3
 			for (String value : special) {
 				if ((index + 2 <= length) && inputStr.substring(index, index + 2).equals(value)) {
 					index = index + 2;

4da41cf02c04c6ba73f4315a18a713e0b3d1aab7
eunji.park <pej870922@gmail.com>
2023-05-31 16:31:10 +0200

    quize

---
+++ src/main/java/com/quize/caculator/Main_1712.java
@@ -0,0 +1,30 @@
+package com.quize.caculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author https://www.acmicpc.net/problem/1712
+ *
+ */
+public class Main_1712 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int fixedCost = Integer.parseInt(st.nextToken());
+		int variableCost = Integer.parseInt(st.nextToken());
+		int sellingPrice = Integer.parseInt(st.nextToken());
+
+		if(variableCost < sellingPrice) {
+			//Need to find a point time for profit, so +1
+			System.out.println(fixedCost / (sellingPrice - variableCost) +1);
+		}else {
+			System.out.println(-1);
+		}
+	}
+}
---
+++ src/main/java/com/quize/caculator/Main_2839.java
@@ -0,0 +1,32 @@
+package com.quize.caculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author https://www.acmicpc.net/problem/2839
+ *
+ */
+public class Main_2839 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int target = Integer.parseInt(br.readLine());
+		// case 0:n ==7 or 4 -> -1
+		if (target == 7 || target == 4) {
+			System.out.print(-1);
+		} // case 1: n%5 == 0
+		else if (target % 5 == 0) {
+			System.out.print(target / 5);
+			// case 2: n%5 ==3 or 1 -> n/5 +1
+		} else if (target % 5 == 3 || target % 5 == 1) {
+			System.out.print(target / 5 + 1);
+			// case 3: n%5 ==4 or 2 -> n/5 + 2
+		} else if (target % 5 == 4 || target % 5 == 2) {
+			System.out.print(target / 5 + 2);
+		}
+	}
+}

7d2b8bb01ec4419016417ddf6891d7c561fe7dc1
eunjipark <eunjipark@192.168.45.168>
2023-12-18 17:17:37 +0900

    add problem comment

--- src/main/java/com/leedcode/string/FindIndexFirstOccurrence.java
+++ src/main/java/com/leedcode/string/FindIndexFirstOccurrence.java
@@ -1,4 +1,32 @@
 package com.leedcode.string;
+/*
+28. Find the Index of the First Occurrence in a String
+Easy
+5.2K
+317
+Companies
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+ */
 
 public class FindIndexFirstOccurrence {
 	private static int strStr(String haystack, String needle) {

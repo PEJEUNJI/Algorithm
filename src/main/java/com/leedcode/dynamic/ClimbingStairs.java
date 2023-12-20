6beb3f752c9a40dc8f736adcb5840f38a57070b6
eunjipark <eunjipark@192.168.45.168>
2023-12-20 13:36:41 +0900

    Problem of Dynamic Program

---
+++ src/main/java/com/leedcode/dynamic/ClimbingStairs.java
@@ -0,0 +1,57 @@
+package com.leedcode.dynamic;
+
+/*
+ 70. Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+ */
+public class ClimbingStairs {
+	private static int climbStairs(int n) {
+
+		if (n <= 2)
+			return n;
+
+		int[] dp = new int[n+1];
+		dp[1] = 1; //1 : The way to climb one of stairs is 1.
+		dp[2] = 2; //1,2 : The way to climb one of stairs is 2.
+		//dp[3] = 1+2 //12,21,11 :  The way to climb one of stairs is 3.
+		//dp[4] = 2+2 //22,112,121,211,1111 : 5 (dp[2] + dp[3])
+		//dp[5] = 2+2+1 //221,212,122,1112,1121,1211,2111,11111 : 8 dp[3] + dp[4]
+		
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i-2] + dp[i-1];
+		}
+
+		return dp[n];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(climbStairs(45));
+	}
+
+}
